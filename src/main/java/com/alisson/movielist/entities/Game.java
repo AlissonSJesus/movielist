13975a286f0ac6e037a806da6695a6a3ab3bf8c9
Alisson S de Jesus <albcet@gmail.com>
2023-07-11 11:21:08 -0300

    Realizado o seed dos games na table de testes(h2)

--- src/main/java/com/alisson/movielist/entities/Game.java
+++ src/main/java/com/alisson/movielist/entities/Game.java
@@ -21,23 +21,27 @@ public class Game {
     private Integer year;
     
     private String genre;
-    private String platform;
+    private String platforms;
     private Double score;
     private String imgUrl;
+
+    @Column(length = 500)
     private String shortDescription;
+
+    @Column(length = 1000)
     private String longDescription;
 
     public Game() {
         
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platform, Double score, String imgUrl,
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
             String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
         this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
@@ -76,12 +80,12 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getplatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setplatforms(String platforms) {
+        this.platforms = platforms;
     }
 
     public Double getScore() {
